eae97a4ef68535f0e6853cecf547a9e8af486258
DavidGu <35072445+ChengWeiGu@users.noreply.github.com>
2025-06-27 16:29:28 +0800

    fix_rate_limit_error

--- run_inference.py
+++ run_inference.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Inference for chroma')
     parser.add_argument('-j','--js',type=str, help='jssdk inference, 請輸入query')
     parser.add_argument('-s','--spec', type=str, help='spec inference, 請輸入query')
+    parser.add_argument('-m','--manual', type=str, help='manual inference, 請輸入query')
     args = parser.parse_args()
     
     if args.js:
@@ -17,7 +18,7 @@ if __name__ == '__main__':
         * 2. 組合並獲取 Working Context 
         *'''
         chroma_login_info={
-            "chromadb_path":".\jsobject_chroma",
+            "chromadb_path":"./jsobject_chroma",
             "collection_name":"test_collection",
             "embedding_function":DatabaseProcess.embedding_function}
 
@@ -39,7 +40,7 @@ if __name__ == '__main__':
         * 2. 組合並獲取 Working Context 
         *'''
         chroma_login_info={
-            "chromadb_path":".\spec_chroma",
+            "chromadb_path":"./spec_chroma",
             "collection_name":"test_collection",
             "embedding_function":DatabaseProcess.embedding_function}
 
@@ -49,6 +50,27 @@ if __name__ == '__main__':
         spec_chroma = DatabaseProcess.LangchainChromaDB(chroma_login_info)
         spec_chroma.init_database()
         res_docs = spec_chroma.lc_similarity_search_with_score(query)
+        # working context for LLM later
+        working_context = "\n".join([doc[0].page_content for doc in res_docs])
+        print(working_context)
+    
+    if args.manual:
+        '''**
+        * manual demo
+        * 1. 連線至 chroma
+        * 2. 組合並獲取 Working Context
+        *'''
+        chroma_login_info={
+            "chromadb_path":"./manual_chroma",
+            "collection_name":"test_collection",
+            "embedding_function":DatabaseProcess.embedding_function}
+
+        # test searching
+        # query = "please show me the spec of cMT2158X"
+        query = args.manual
+        manual_chroma = DatabaseProcess.LangchainChromaDB(chroma_login_info)
+        manual_chroma.init_database()
+        res_docs = manual_chroma.lc_similarity_search_with_score(query)
         # working context for LLM later
         working_context = "\n".join([doc[0].page_content for doc in res_docs])
         print(working_context)
